0b6a025076f228ed17f879f287aba754baf2545f
Iuri <lorenzop0o9i8u7@gtmail.com>
2024-12-11 18:12:54 -0400

    inseri comando new line em 11/11/2024

--- MODELAGEM/MODELAGEM_GENERATORS_BDGD_2023_ENERGISA.py
+++ MODELAGEM/MODELAGEM_GENERATORS_BDGD_2023_ENERGISA.py
@@ -46,17 +46,12 @@ class DatabaseQuery:
             # Consulta a tabela SSDMT para extrair as colunas especificadas
             query = """
                 SELECT 
-                        ugbt_tab.cod_id, ugbt_tab.pac, ugbt_tab.ctmt, ugbt_tab.fas_con,
-                        ugbt_tab.ten_con, ugbt_tab.pot_inst, ugbt_tab.cep,
-                        ugbt_tab.ceg_gd,
-                        ugbt_tab.ene_01, ugbt_tab.ene_02, ugbt_tab.ene_03,
-                        ugbt_tab.ene_04, ugbt_tab.ene_05, ugbt_tab.ene_06,
-                        ugbt_tab.ene_07, ugbt_tab.ene_08, ugbt_tab.ene_09,
-                        ugbt_tab.ene_10, ugbt_tab.ene_11, ugbt_tab.ene_12,
-                        ugbt_tab.dem_cont
+                        ugmt_tab.cod_id, ugmt_tab.pac, ugmt_tab.ctmt, ugmt_tab.fas_con,
+                        ugmt_tab.ten_con, ugmt_tab.pot_inst, ugmt_tab.cep,
+                        ugmt_tab.ceg_gd,
 
                 FROM 
-                    ugbt_tab;       
+                    ugmt_tab;       
             """
             # Executa a consulta
             self.cur.execute(query)
@@ -85,7 +80,7 @@ class DatabaseQuery:
             ten_con = linha[4]
             potencia_instalada_kwp = linha[5]
             cep = linha[6]
-            potencia_max_inversor_kw = linha[5]
+            potencia_max = linha[5]
             energia_desejada = linha[8:20]
             demanda_contratada = linha[20]
             ceg_gd = linha[7]
@@ -96,171 +91,41 @@ class DatabaseQuery:
                     ctmt_folder = os.path.join(base_dir, str(ctmt))
                     os.makedirs(ctmt_folder, exist_ok=True)
 
-                for index_mensal, mensal in enumerate(energia_desejada):
-                    """ Para cada energia gerada mensalmente criar um arquivo de curva de geração"""
-                    mensal_folder = os.path.join(ctmt_folder, str(f'geração_shape_mes_{index_mensal + 1}'))
-                    os.makedirs(mensal_folder, exist_ok=True)
+                file_path = os.path.join(ctmt_folder, 'Generators.dss')
+                file = open(file_path, 'w')
 
-                    for index_dia in range(30):
-                        diario_filename = f'geração_shape_dia_{index_dia + 1}.txt'
-                        file_path = os.path.join(mensal_folder, diario_filename)
+                # Adicionar o ctmt ao dicionário de ctmts processados (armazena o arquivo aberto)
+                ctmts_processados[ctmt] = file
 
-                        with open(file_path, 'a') as file:
-                            """
-                                  IMPLEMENTAÇÃO DE TODAS AS CURVAS PARA PAINEIS SOLARES
-                              """
+                with open(file_path, 'a') as file:
+                    ten = 13.8
+                    if ten_con == 49:
+                        ten = 13.8
+                    if ten_con == 72:
+                        ten = 34.5
 
-                            def calcular_potencia_gerada(irradiancia, potencia_instalada_kwp, eficiencia):
-                                """
-                                :param irradiancia: quantidade em wh/m2 de sol
-                                :param potencia_instalada_kwp: potência do inversor solar
-                                :param eficiencia: limitações e saturações do inversor
-                                :return:
-                                """
-                                potencia = potencia_instalada_kwp * (irradiancia / 1000) * eficiencia
-                                return potencia
-
-                            def aplicar_saturacao_inversor(potencia_gerada, potencia_max_inversor_kw):
-                                """
-                                :param potencia_gerada: diferência entre a quantidade de potência que entra - perdas, saturações...
-                                :param potencia_max_inversor_kw: maior quantidade de potência que o inversor consegue injetar nna rede
-                                :return:
-                                """
-                                return np.minimum(potencia_gerada, potencia_max_inversor_kw)
-
-                            def estimar_temperatura(hora, latitude):
-                                """
-                                :param hora: hora do dia de simulação do respectivo ano escolhido
-                                :param latitude:  a temperatura varia com a latitude (distancia até a linha do Equador)
-                                :return:
-                                """
-                                t_max = 40
-                                t_min = 15
-                                temperatura = (t_max + t_min) / 2 + (t_max - t_min) / 2 * np.sin(np.pi * (hora - 6) / 12)
-                                return temperatura
-
-                            def calcular_irradianca_temperatura_desempenho(latitude, longitude, altitude,
-                                                                           potencia_instalada_kwp, eficiencia,
-                                                                           potencia_max_inversor_kw, energia_desejada):
-                                """
-                                :param latitude: distância da linha do Equador
-                                :param longitude: distância do Grenwich
-                                :param altitude: altura do ponto escolhido em relação ao nivel do mar
-                                :param potencia_instalada_kwp: potência do inversor
-                                :param eficiencia: do inversor
-                                :param potencia_max_inversor_kw: maior potencia despachada do inversor
-                                :param energia_desejada: energia calculada horariamente para resultar na energia mensal da BDGD
-                                :return:
-                                """
-
-                                site = location.Location(latitude, longitude, altitude=400)
-
-                                rec_mensal = 0
-                                rec_diario = 0
-                                if int(index_mensal) < 1:
-                                    rec_mensal = index_mensal
-                                    rec_mensal = 1
-
-                                if int(index_dia) < 1:
-                                    rec_diario = index_dia
-                                    rec_diario = 1
-
-                                # Ajuste para garantir que rec_diario nunca seja 0
-                                index_3 = f'{int(rec_mensal):02d}' if rec_mensal > 0 else '01'  # Garante que o mês não seja 0
-                                index_4 = f'{int(rec_diario):02d}' if rec_diario > 0 else '01'  # Garante que o dia não seja 0
-
-                                times = pd.date_range(f'2023-{index_3}-{index_4}', f'2023-{index_3}-{index_4} 23:59', freq='15min', tz='America/Cuiaba')
-
-                                """Calcular a irradiança global horizontal (GHI), difusa (DHI) e direta (DNI)"""
-                                solar_position = site.get_solarposition(times)
-                                irradiance = site.get_clearsky(times)
-
-                                """ Estimar a temperatura para cada 15 minutos"""
-                                temperatura = [estimar_temperatura(minutos.hour, latitude) for minutos in times]
-
-                                """Calcular a potência gerada com base na irradiância (GHI)"""
-                                potencia_gerada = calcular_potencia_gerada(irradiance['ghi'], potencia_instalada_kwp,
-                                                                           eficiencia)
-
-                                """Ajuste da potência gerada para atingir a energia desejada"""
-                                energia_total_gerada = potencia_gerada.sum()
-                                fator_ajuste = (energia_desejada[index_mensal] / 30) / energia_total_gerada
-
-                                """Ajustar a potência gerada para atingir a energia desejada"""
-                                potencia_gerada_ajustada = potencia_gerada * fator_ajuste
-
-                                """Aplicar a saturação do inversor"""
-                                potencia_gerada_limitada = aplicar_saturacao_inversor(potencia_gerada_ajustada,
-                                                                                      potencia_max_inversor_kw)
-
-                                return irradiance['ghi'].tolist(), temperatura, potencia_gerada_ajustada.tolist(), potencia_gerada_limitada.tolist()
-
-                            # """Conversão para latitude e Longitude"""
-                            # def obter_coordenadas_por_cep(cep):
-                            #     geolocator = Nominatim(user_agent="meu_script_de_geolocalizacao_2023")
-                            #
-                            #     """O geocoder retorna um objeto com as informações do local"""
-                            #     location = geolocator.geocode(cep, timeout=10)
-                            #
-                            #     if location:
-                            #         # Retorna a latitude e Longitude
-                            #         return location.latitude, location.longitude
-                            #     else:
-                            #         return None, None
-
-                            """ As coordenadas foram baseadas em Cuiaba para os calculos"""
-                            latitude = -15.59583
-                            longitude = -56.09694
-                            altitude = 400
-                            """ Eficiencia media de uma painel fotovoltaico """
-                            eficiencia = 0.18
-
-                            irradiance, temperatura, potencia_gerada_ajustada, potencia_gerada_limitada  = calcular_irradianca_temperatura_desempenho(
-                                latitude,
-                                longitude, altitude,
-                                potencia_instalada_kwp, eficiencia,
-                                potencia_max_inversor_kw, energia_desejada)
-
-                            """ Cálculo da eficiência em função da temperatura para os inversores solares """
-                            gamma = -0.004                                   # Coeficiente de temperatura do painel (em %/°C)
-                            alpha = 0.002                                    # Coeficiente de variação da eficiência do inversor em (em %/°C)
-                            eficiencia_maxima_inversor = 0.98
-                            temperatura_referencia = [25 for _ in range(96)]
-                            potencia_corrigida = [p_g_l * (1 + gamma * (t - t_r))
-                                                  for t, t_r, p_g_l in zip(temperatura, temperatura_referencia, potencia_gerada_limitada)]
-
-                            """ Calculando a eficiencia do inversor em função da temperatura """
-                            eficiencia_inversor = [eficiencia_maxima_inversor - alpha * (t - t_r)
-                                                   for t, t_r in zip(temperatura, temperatura_referencia)]
+                    mapa_fases = {
+                        'ABC': '.1.2.3', 'ACB': '.1.3.2', 'BAC': '.1.2.3', 'BCA': '.1.2.3', 'CAB': '.1.2.3',
+                        'CBA': '.1.2.3',
+                        'ABCN': '.1.2.3', 'ACBN': '.1.2.3', 'BACN': '.1.2.3', 'BCAN': '.1.2.3', 'CABN': '.1.2.3',
+                        'CBAN': '.1.2.3',
+                        'ABN': '.1.2', 'ACN': '.1.3', 'BAN': '.1.2', 'CAN': '.1.3', 'A': '.1', 'B': '.2', 'C': '.3',
+                        'AN': '.1',
+                        'BA': '.1.2', 'BN': '.2', 'CN': '.3', 'AB': '.1.2', 'AC': '.1.3', 'BC': '.2.3',
+                        'CNA': '.1', 'ANB': '.1.2', 'BNC': '.2.3', 'CA': '.1.3',
+                    }
+                    rec_fases = mapa_fases[fas_con]
 
 
-                            """ """
+                    command_pvsystem = f"""
+                    ! CTMT - {ctmt}
+                                
+                            New Generator.{cod_id} Bus1 = {pac}{rec_fases} KW = {potencia_max} Model = {1} PF = 1 KVA = {potencia_max} KV = {ten} Xdp = {0.27} xdpp = {0.20} H = {2}
+                            ~ Conn=Delta
+                     """
 
-                            # command_pvsystem = f"""
-                            #     eficiencia_vs_temperatura = {}
-                            #     eficiencia_vs_potencia_inversor = {}
-                            #     valores_irradianca_intervalos_15m_diario = {}
-                            #     valores_temperatura_intervalos_15m_diario = {}
 
-                            #   """
-                            potencia_pu = [
-                                pot_limitada / pot_ajustada if pot_ajustada != 0 else 0
-                                for pot_limitada, pot_ajustada in zip(potencia_gerada_limitada, potencia_gerada_ajustada)
-                            ]
-                            ten = 13.8
-                            if ten_con == 49:
-                                ten = 13.8
-                            if ten_con == 72:
-                                ten = 34.5
-                            command_pvsystem = f"""
-                                New xycurve.mypvst_{cod_id} npts = {96} xarray = {[temperatura]} yarray = {[eficiencia_inversor]} !curva de desempenho do painel em função da temperatura (colocar uma curva constante )
-                                New xycurve.myeff_{cod_id} npts = {96} xarray = {[potencia_pu]} yarray = {[eficiencia_inversor]}  ! eficiência do sistema para diferentes valores de carga
-                                New loadshape.myirrad_{cod_id} npts = {96} interval = 15min mult = {[irradiance]} ! distribui os valores da irradição solar ao longo das 24 horas do dia e noite
-                                New tshape.mytemp_{cod_id} npts = {96} interval = 15min temp = {[temperatura]}  ! define a temperatura ambiente ao longo das 24 horas
-                                New pvsystem.pv_{cod_id} phases = {len(fas_con)} conn = estrela bus1 = {pac} kv = {ten} kva = {max(potencia_gerada_ajustada)} pmpp = {max(potencia_gerada_ajustada)} pf = {1} %cutin = {0.00005} %cutout = {0.00005} varfollowinverter = Yes effcurve = myeff_{cod_id} p-tcurve = mypvst_{cod_id} daily = myirrad_{cod_id} tdaily = mytemp_{cod_id}
-                                """
-
-                            file.write(command_pvsystem + "\n")
+                    file.write(command_pvsystem + "\n")
 
     def close(self):
         """Fecha a conexão com o banco de dados"""
